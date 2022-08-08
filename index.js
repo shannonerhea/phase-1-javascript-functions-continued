8cff37cd8c70f756df7d4e9ec17f8acf39b687dd
shannone <shannone426@gmail.com>
2022-08-08 11:52:10 -0400

    W1D1 functions

--- index.js
+++ index.js
@@ -12,6 +12,9 @@ function saturdayFun (activity = "roller-skate") {
 
 
 //1) uses the default activity 'go to the office' when no arguments are passed
-function mondayWork (activity = "go to the office.") {
-    return `This Monday, I will ${activity}`
+function mondayWork (activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
 };
+
+//when initialized with '*' creates a function that, when called, wraps an adjective in a highlight
+function wrapAdjective () {};
